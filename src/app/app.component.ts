4ccdfda7418811bba0080fb696b29a9c745a5778
rohman17 <rohman.ariel@gmail.com>
2017-04-13 17:13:12 +0700

    belajar databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+	<h1>Inline Template</h1>	
+	<fa-databinding></fa-databinding>
+  `,
+  styles: [`
+	h1{
+		color:blue;
+	}
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'tes brrooo!';
 }
---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+//import {PropertyBindingComponent} from './property-binding.component';
+
+@Component({
+  selector: 'fa-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css'],
+  //directives:[PropertyBindingComponent]
+})
+export class DatabindingComponent{
+	stringInterpolation = 'String Inter';
+	numberInterpolation = 2;
+	
+	onTest()
+	{
+		return true;
+	}
+	
+	onClicked(value : string)
+	{
+		alert(value);
+	}
+}
